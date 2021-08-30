065454e9a935fbace10b6ef12a0126c4ecf33bf8
Filipe Garcia <garcia.filipeandre@gmail.com>
2021-08-30 23:43:05 +0100

    Add semgrep & workflow

--- run_tests.sh
+++ run_tests.sh
@@ -7,5 +7,8 @@ black --skip-string-normalization --line-length 120 src
 # run python static validation
 prospector --profile-path=. --profile=.prospector.yml --path=src --ignore-patterns=static
 
+# run semgrep
+semgrep --config p/python src
+
 # run tests
 py.test -n 4 --disable-socket -W error::RuntimeWarning --cov=src --cov-report=html --cov-fail-under=100 tests/
